87149d48d027c121003be815301224df9289b6bf
packrunner <kuhnc@udel.edu>
2023-02-20 22:38:53 -0500

    cleaner

--- answer.py
+++ answer.py
@@ -4,31 +4,18 @@ Updated: 2/13/2023
 Description: A sample of taking a filename from stdin, loading it into a string
              array, and printing out the first line.
 """
-def addNumbers(nums: list[int]):
-    i=1
-    temp=[]
-    if(len(nums)==0):
-        return None
-    for num in range(i,(len(nums))):
-        if(int(nums[i]==-999)):
-            break
-        elif(int(nums[i])<0):
-            i+=1
-        else:
-            temp.append(int(nums[i]))
-            i+=1
-    if(len(temp)==0):
-        return "EMPTY"
-    final = sum(temp)
-    return final
-def print_all_lines(lines: list[str]) -> str:
-    """ Print each line, then return the last one. """
-    latest_line = None
+def addNumbers(lines: list[str]):
+    sum = 0
     for line in lines:
-        # Readlines includes the newline character
-        latest_line = line.strip()
-        print(latest_line)
-    return latest_line
+        current_line = int(line)
+        if current_line >0:
+            sum+=current_line
+        elif current_line == -999:
+            break
+    return sum
+                
+        
+
 
 
 if __name__ == "__main__":
